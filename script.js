75692a87c54a7eeaea4a9ae5493ef324d3e01c79
yismail01 <yismail3110@gmail.com>
2022-06-22 13:46:33 -0400

    Implemented highscore

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,11 +26,15 @@ document.querySelector('.check').addEventListener('click', function () {
     //when player wins
   } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number!';
-    score++;
     document.querySelector('.number').textContent = number;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     //guess too high
   } else if (guess > number) {
